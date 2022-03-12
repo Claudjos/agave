621bd209f5a0eed30660b12b99b01db45c0f4943
Claudjos <claudjosmail@gmail.com>
2022-03-12 21:15:13 +0100

    Increased isolation of functions in arp.listen

--- agave/modules/arp/listen.py
+++ agave/modules/arp/listen.py
@@ -1,6 +1,7 @@
 from agave.frames import ethernet, arp
 from agave.frames.core import Buffer
 from ipaddress import ip_address, ip_network, IPv4Address
+from typing import Iterator, Tuple
 import socket
 import select
 
@@ -18,7 +19,7 @@ def push_data(ip: str, mac: str):
 			print(f"[UPDATE] {ip} @ {mac}", flush=True)
 
 
-def process(buf: Buffer):
+def process(buf: Buffer) -> Iterator[Tuple[str, str]]:
 	"""
 	Collects data from ARP messages. Target address are collected just
 	for replies.
@@ -27,28 +28,32 @@ def process(buf: Buffer):
 	if eth_frame.next_header == ethernet.ETHER_TYPE_ARP:
 		arp_frame = arp.ARP.read_from_buffer(buf)
 		if arp_frame.operation == arp.OPERATION_REPLY:
-			push_data(
+			yield (
 				str(ip_address(arp_frame.target_protocol_address)),
 				ethernet.mac_to_str(arp_frame.target_hardware_address)
 			)
-		push_data(
+		yield (
 			str(ip_address(arp_frame.sender_protocol_address)),
 			ethernet.mac_to_str(arp_frame.sender_hardware_address)
 		)
+	return
 
 
-def loop(rawsocket, timeout):
-	print("Listening for ARP messages...")
-	while True:
-		rl, wl, xl = select.select([rawsocket], [], [], timeout)
-		if rl != []:
-			process(Buffer.from_bytes(rawsocket.recv(65535)))
+def read(rawsocket, timeout) -> Iterator[Tuple[str, str]]:
+	rl, wl, xl = select.select([rawsocket], [], [], timeout)
+	if rl != []:
+		for item in process(Buffer.from_bytes(rawsocket.recv(65535))):
+			yield item
+	return
 
 
 def main(argv):
 	try:
 		rawsocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
 		timeout = 1
-		loop(rawsocket, timeout)
+		print("Listening for ARP messages...")
+		while True:
+			for item in read(rawsocket, timeout):
+				push_data(item[0], item[1])
 	except KeyboardInterrupt as e:
 		pass

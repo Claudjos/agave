472467a318b3f13621ff2706a287f21c22e50bdb
Claudjos <claudjosmail@gmail.com>
2022-03-13 22:53:53 +0100

    Added more information to interfaces

--- agave/modules/info/iface.py
+++ agave/modules/info/iface.py
@@ -1,27 +1,50 @@
 import socket
 import fcntl
 import struct
+from ipaddress import ip_address, ip_network
+
+"""
+From <bits/ioctls.h>
+"""
+SIOCGIFADDR = 0x8915 		# get PA address
+SIOCGIFBRDADDR = 0x8919 	# get broadcast PA address
+SIOCGIFNETMASK = 0x891b 	# get network PA mask
+SIOCGIFHWADDR = 0x8927 		# get HW address
 
 
 def get_addresses(s, ifname: str):
 	 
 		iface = struct.pack('256s', bytes(ifname, 'utf-8')[:15])
-		
 
-		eth = fcntl.ioctl(s.fileno(), 0x8927, iface)[18:24]
+		eth = fcntl.ioctl(s.fileno(), SIOCGIFHWADDR, iface)[18:24]
 		eth2 = ":".join('%02x' % b for b in eth)
 
 		try:
-			ip = fcntl.ioctl(s.fileno(), 0x8915, iface)[20:24]
+			ip = fcntl.ioctl(s.fileno(), SIOCGIFADDR, iface)[20:24]
 			ip2 = socket.inet_ntoa(ip)
+
+			broadcast = socket.inet_ntoa(fcntl.ioctl(s.fileno(), SIOCGIFBRDADDR, iface)[20:24])
+			netmask = fcntl.ioctl(s.fileno(), SIOCGIFNETMASK, iface)[20:24]
+			
+			network = ip_address(int.from_bytes(netmask, byteorder="big") & int.from_bytes(ip, byteorder="big"))
+
+			n = ip_network("{}/{}".format(
+				str(network),
+				socket.inet_ntoa(netmask)
+			)).prefixlen
+
+			network = f"{network}/{n}"
+
 		except:
-			ip = ip2 = None
+			ip = ip2 = broadcast = network = None
 
 		return {
 			"eth": eth,
 			"formatted_eth": eth2,
 			"ip": ip,
-			"formatted_ip": ip2
+			"formatted_ip": ip2,
+			"broadcast": broadcast,
+			"network": network
 		}
 
 
@@ -40,12 +63,19 @@ def interfaces():
 
 
 def main(args):
-	print("{:20}\t{}\t{:20}\t{}\t".format("NAME", "ID", "ETHER ADDR", "IP ADDR"))
-	print("---------------------------------------------------------------------")
-	for iface in interfaces():
-		print("{:20}\t{}\t{}\t{}\t".format(
-			iface.get("name"),
-			iface.get("id"),
-			iface.get("formatted_eth"),
-			iface.get("formatted_ip", "")
-		))
+	if len(args) > 0:
+		e = list(filter(lambda x: x.get("name") == args[0], interfaces()))
+		if len(e) < 1:
+			print("Interfaces not found")
+		else:
+			for key, value in e[0].items():
+				print("{:20}\t{}".format(key, value))
+	else:
+		print("{:20}\t{:20}\t{}\t".format("NAME", "ETHER ADDR", "IP ADDR"))
+		print("---------------------------------------------------------------------")
+		for iface in interfaces():
+			print("{:20}\t{}\t{}\t".format(
+				iface.get("name"),
+				iface.get("formatted_eth"),
+				iface.get("formatted_ip", "")
+			))

93ab3e0e082bdf6915156b0c1f12c68061ca2c36
Claudio Santoro <58224922+Claudjos@users.noreply.github.com>
2022-04-03 12:32:05 +0200

    ICMPv6 (#5)
    Added header definitions for IPv6 and ICMPv6.

---
+++ agave/core/icmpv6.py
@@ -0,0 +1,107 @@
+"""Defines the ICMPv6 protocol as for RFC 4443."""
+from .frame import FrameWithChecksum, compute_checksum_from_buffer
+from .buffer import Buffer
+
+
+TYPE_ECHO_REQUEST = 128
+TYPE_ECHO_REPLY = 129
+
+# NDP Message types
+TYPE_ROUTER_SOLICITATION = 133
+TYPE_ROUTER_ADVERTISEMENT = 134
+TYPE_NEIGHBOR_SOLICITATION = 135
+TYPE_NEIGHBOR_ADVERTISEMENT = 136
+TYPE_REDIRECT_MESSAGE = 137
+
+
+class ICMPv6(FrameWithChecksum):
+	"""ICMPv6 message, RFC 4443.
+
+	Attributes:
+		_type: ICMP message type
+		code: ICMP message code
+		checksum: ICMP message checksum
+		body: ICMP message
+		payload: rest of ICMP message
+		_pseudo_header: IPv6 pseudo header for checksum computation.
+
+	"""
+	__slots__ = ("_type", "code", "checksum", "body", "payload", "_pseudo_header")
+
+	def __init__(
+		self,
+		_type: int,
+		code: int,
+		checksum: int,
+		body: int,
+		payload: bytes = b''
+	):
+		self.type: int = _type
+		self.code: int = code
+		self.checksum: int = checksum
+		self.body: int = body
+		self.payload: bytes = payload
+		self._pseudo_header : bytes = b''
+
+	@classmethod
+	def read_from_buffer(cls, buf: Buffer) -> "ICMPv6":
+		"""Parses an ICMPv6 message from a Buffer.
+
+		Args:
+			buf: the buffer.
+
+		Returns:
+			An instance of this class.
+
+		"""
+		return cls(
+			buf.read_byte(),
+			buf.read_byte(),
+			buf.read_short(),
+			buf.read_int(),
+			buf.read_remaining()
+		)
+
+	def write_to_buffer(self, buf: Buffer):
+		"""Writes this ICMPv6 message to a buffer.
+
+		Args:
+			buf: the buffer.
+
+		"""
+		buf.write_byte(self.type)
+		buf.write_byte(self.code)
+		buf.write_short(self.checksum)
+		buf.write_int(self.body)
+		buf.write(self.payload)
+
+	def set_pseudo_header(self, header: bytes):
+		"""Sets the pseudo header to use when
+		calculating checksum.
+		
+		Args:
+			header: the pseudo header.
+
+		"""
+		self._pseudo_header = header
+
+	def compute_checksum(self) -> int:
+		"""Compute the checksum for this message.
+
+		Returns:
+			The checksum for this message.
+
+		"""
+		# Writes header to buffer
+		buf = Buffer.from_bytes()
+		buf.write(self._pseudo_header)
+		self.write_to_buffer(buf)
+		words = 4 + int(len(self.payload) / 2) + int(len(self._pseudo_header) / 2)
+		# Pads (?)
+		if len(self.payload) % 2 == 1:
+			buf.write_byte(0)
+			words +=1
+		buf.rewind()
+		# Compute
+		t = compute_checksum_from_buffer(buf, words)
+		return t
--- agave/core/ip.py
+++ agave/core/ip.py
@@ -1,11 +1,25 @@
-from .frame import FrameWithChecksum
+from .frame import Frame, FrameWithChecksum
 from .buffer import Buffer
-from ipaddress import ip_address, IPv4Address
+from ipaddress import ip_address, IPv4Address, IPv6Address
 
 
 PROTO_ICMP = 0x01
 PROTO_TCP = 0x06
 PROTO_UDP = 0x11
+PROTO_ICMPv6 = 0x3A
+
+PROTO_IPv6_HOPOPT = 0x00
+PROTO_IPv6_ROUTE = 0x2B
+PROTO_IPv6_FRAG = 0x2C
+PROTO_IPv6_NoNXT = 0x3B
+PROTO_IPv6_OPTS = 0x3C
+
+IPv6_EXTENSION = [
+	PROTO_IPv6_ROUTE,
+	PROTO_IPv6_FRAG,
+	PROTO_IPv6_NoNXT,
+	PROTO_IPv6_OPTS
+]
 
 
 class IPv4(FrameWithChecksum):
@@ -122,3 +136,148 @@ class IPv4(FrameWithChecksum):
 		ip_frame.write_to_buffer(buf)
 		buf.write(payload)
 		return bytes(buf)
+
+
+class IPv6(Frame):
+	"""IPv6 message, RFC 8200.
+
+	Attributes:
+		version: IPv6 version
+		traffic_class: IPv6 traffic class.
+		flow_label: IPv6 flow label.
+		payload_length: IPv6 payload length.
+		next_header: IPv6 next header.
+		hop_limit: IPv6 hop limit.
+		source: IPv6 source.
+		destination: IPv6 destination.
+		extensions: unparsed extension.
+	
+	Todo:
+		* handle extension headers.
+
+	"""
+	__slots__ = (
+		"traffic_class", "flow_label", "payload_length", "next_header",
+		"hop_limit", "source", "destination", "extensions", "version"
+	)
+
+	def __init__(
+		self,
+		traffic_class: int,
+		flow_label: int,
+		payload_length: int,
+		next_header: int,
+		hop_limit: int,
+		source: IPv6Address,
+		destination: IPv6Address,
+		extensions: bytes = None
+	):
+		self.version: int = 6
+		self.traffic_class: int = traffic_class
+		self.flow_label: int = flow_label
+		self.payload_length: int = payload_length
+		self.next_header: int = next_header
+		self.hop_limit: int = hop_limit
+		self.source: IPv6Address = source
+		self.destination: IPv6Address = destination
+		self.extensions: bytes = b''
+
+	@classmethod
+	def read_from_buffer(cls, buf: Buffer) -> "IPv6":
+		"""Parses an IPv6 header from a buffer.
+
+		Args:
+			buf: the buffer.
+
+		Returns:
+			An instance of this class.
+
+		Todo:
+			* parse extension headers
+
+		"""
+		temp = buf.read_int()
+		traffic_class = (temp & 0x0ff00000) >> 20
+		flow_label = temp & 0x000fffff
+		payload_length = buf.read_short()
+		next_header = buf.read_byte()
+		hop_limit = buf.read_byte()
+		source = IPv6Address(buf.read(16))
+		destination = IPv6Address(buf.read(16))
+		return cls(
+			traffic_class,
+			flow_label,
+			payload_length,
+			next_header,
+			hop_limit,
+			source,
+			destination
+		)
+
+	def write_to_buffer(self, buf: Buffer):
+		"""Writes this message headers on a buffer.
+
+		Args:
+			buf: the buffer.
+
+		"""
+		buf.write_int(0x60000000 | (self.traffic_class << 20) | self.flow_label)
+		buf.write_short(self.payload_length)
+		buf.write_byte(self.next_header)
+		buf.write_byte(self.hop_limit)
+		buf.write(self.source.packed)
+		buf.write(self.destination.packed)
+		buf.write(self.extensions)
+
+	def __str__(self):
+		return "({}) {} -> {}".format(
+			self.next_header,
+			self.source,
+			self.destination
+		)
+
+	def get_pseudo_header(self) -> bytes:
+		"""Builds the pseudo header for this IPv6 message.
+
+		Returns:
+			The pseudo header.
+
+		Todo:
+			* replace next_header with upper layer protocol, or
+				this will break with extensions.
+			* replace payload_length with upper protocol packet
+				size, or this will break with extensions.
+
+		"""
+		return self.build_pseudo_header(
+			self.source,
+			self.destination,
+			self.payload_length,
+			self.next_header
+		)
+
+	def build_pseudo_header(
+		self,
+		source: IPv6Address,
+		destination: IPv6Address,
+		packet_length: int,
+		next_header: int
+	) -> bytes:
+		"""Builds the pseudo header necessary to upper protocols
+		for checksum calculation.
+
+		Args:
+			source: source address.
+			destination: destination address.
+			packet_length: upper layer header and data size.
+			next_header: upper layer protocol.
+
+		"""
+		return (
+			source.packed + 
+			destination.packed + 
+			packet_length.to_bytes(4, byteorder="big") +
+			b'\x00\x00\x00' +
+			next_header.to_bytes(1, byteorder="big")
+		)
+

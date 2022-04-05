b0f9492b008ce8f01c75e5832714506f6358d93e
Claudjos <claudjosmail@gmail.com>
2022-04-05 18:05:39 +0200

    Improved tests organization

--- tests/v4/test_arp_spoof.py
+++ tests/v4/test_arp_spoof.py
@@ -1,38 +1,14 @@
 import unittest
 from tests.dummy import DummyInterface, DummySock
-from agave.core.arp import *
+from agave.core.arp import ARP, OPERATION_REQUEST
 from agave.core.buffer import Buffer
 from agave.core.ethernet import MACAddress, ETHER_TYPE_ARP
-from agave.arp.resolve import Resolver
 from agave.arp.spoof import Spoofer
 from agave.arp.utils import create_filter, _parse
 from ipaddress import IPv4Network, IPv4Address 
 
 
-class TestARPJobs(unittest.TestCase):
-
-	def test_resolve(self):
-		"""Process should return result for given IP only once; replies for data outside
-		the scanned subnet should be ignored."""
-		job = Resolver(None, DummyInterface("eth0"), IPv4Network("192.168.0.0/24"), 1)
-		# build replies
-		valid_reply = ARP.is_at(
-			MACAddress("00:1c:f2:be:4d:14"), IPv4Address("192.168.0.1"),
-			MACAddress("22:11:33:be:4d:14"), IPv4Address("192.168.0.2")
-		)
-		invalid_reply = ARP.is_at(
-			MACAddress("11:1c:f2:be:4d:14"), IPv4Address("192.168.1.1"),
-			MACAddress("22:11:33:be:4d:14"), IPv4Address("192.168.0.2")
-		)
-		# process reply
-		result = job.process(valid_reply, ("eth0", 0x0806, 1, 1, b'\x00\x1c\xf2\xbe\x4d\x14'))
-		self.assertEqual(result, (MACAddress("00:1c:f2:be:4d:14"), IPv4Address("192.168.0.1")))
-		# process the same reply
-		result = job.process(valid_reply, ("eth0", 0x0806, 1, 1, b'\x00\x1c\xf2\xbe\x4d\x14'))
-		self.assertEqual(result, None)
-		# process invalid reply
-		result = job.process(invalid_reply, ("eth0", 0x0806, 1, 1, b'\x11\x1c\xf2\xbe\x4d\x14'))
-		self.assertEqual(result, None)
+class TestARPSpoof(unittest.TestCase):
 
 	def test_spoof(self):
 		"""Should reply back with poisoned data."""
@@ -59,3 +35,4 @@ class TestARPJobs(unittest.TestCase):
 		self.assertEqual(reply.sender_protocol_address, IPv4Address("192.168.1.2").packed)
 		self.assertEqual(reply.target_hardware_address, MACAddress("00:1c:f2:be:4d:14").packed)
 		self.assertEqual(reply.target_protocol_address, IPv4Address("192.168.1.1").packed)
+

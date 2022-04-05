742f3edf8ae8508f8ee6524b8eaeb8f6329693f4
Claudjos <claudjosmail@gmail.com>
2022-04-05 23:41:40 +0200

    icmp.discover: removed NetworkScanner; simplified response cache.

--- agave/icmp/discover.py
+++ agave/icmp/discover.py
@@ -1,91 +1,34 @@
-"""ICMPv4 host discover utilities.
+"""ICMPv4 Echo request/reply PING.
 
-Todo:
-	* clean up this mess.
-
-The module provides a script to discover host in a network
-by using Echo ICMPv4 messages.
+The module provides a script to discover host in a network by 
+using Echo ICMPv4 messages. With the option -m, returns also
+the host for which neither a reply or a destination unreachable 
+message was received.
 
 Usage:
-	python3 -m agave.icmp.discover <subnet>
+	python3 -m agave.icmp.discover <subnet> [-m]
 
 Example:
 	python3 -m agave.icmp.discover 192.168.1.0/24
+	python3 -m agave.icmp.discover 192.168.1.0/24 -m
 
 """
+import socket
 from agave.core.helpers import Job, SocketAddress
-from agave.core import ethernet, ip, icmpv4 as icmp
 from agave.core.icmpv4 import ICMPv4, TYPE_ECHO_REPLY, TYPE_DESTINATION_UNREACHABLE
 from agave.core.ip import IPv4
-from agave.core.buffer import Buffer
-from ipaddress import ip_address, ip_network
-import socket
-from typing import List
-
-
-class NetworkReport:
-
-	STATUS_REACHED = 1
-	STATUS_UNREACHABLE = 2
-	STATUS_UNKNOWN = 3
-
-	def __init__(self, subnet: str):
-		self.subnet = ip_network(subnet)
-		self.all_hosts = list(map(lambda x: x._ip, self.get_hosts()))
-		self.hosts_status = [(self.STATUS_UNKNOWN, None)] * len(self.all_hosts)
-
-	def get_hosts(self):
-		for address in self.subnet.hosts():
-			yield address
-		return
-
-	def get_index(self, ip: bytes):
-		try:
-			return self.all_hosts.index(ip_address(ip)._ip)
-		except ValueError:
-			return None
-
-	def set_unreachable(self, ip: bytes):
-		index = self.get_index(ip)
-		if index != None:
-			self.hosts_status[index] = (self.STATUS_UNREACHABLE, None)
-
-	def set_reached(self, ip: bytes, ttl: int):
-		index = self.get_index(ip)
-		if index != None:
-			self.hosts_status[index] = (self.STATUS_REACHED, ttl)
-
-	def build_echo_requests(self, repeat: int = 2):
-		for _ in range(0, repeat):
-			for address in self.all_hosts:
-				t = icmp.ICMPv4.echo(b"abcdefghijklmonpqrstuvwxyz")
-				t.set_checksum()
-				yield bytes(t), (str(ip_address(address)), 0)
-		return
-
-	def get_confirmed_host(self):
-		out = []
-		for i in range(0, len(self.hosts_status)):
-			status, ttl = self.hosts_status[i]
-			if status == self.STATUS_REACHED:
-				out.append("{} {}".format(str(ip_address(self.all_hosts[i])), str(ttl)))
-		return out
-
-	def get_missing_host(self):
-		out = []
-		for i in range(0, len(self.hosts_status)):
-			status, ttl = self.hosts_status[i]
-			if status == self.STATUS_UNKNOWN:
-				out.append("{}".format(str(ip_address(self.all_hosts[i]))))
-		return out
+from ipaddress import IPv4Address, IPv4Network, ip_network
+from typing import List, Tuple, Iterator
 
 
 class Pinger(Job):
 
-	def __init__(self, sock: "socket.socket", report: NetworkReport, **kwargs):
-		super().__init__(sock, wait=0.5, **kwargs)
-		self.report = report
-		self.packets_to_send = report.build_echo_requests()
+	def __init__(self, sock: "socket.socket", subnet: IPv4Network, repeat: int = 2, **kwargs):
+		super().__init__(sock, **kwargs)
+		self.subnet = subnet
+		self.packets_to_send = self.generate_echo_requests(subnet, repeat)
+		self._cache = set()
+		self._count = self.subnet.num_addresses
 
 	def loop(self) -> bool:
 		for message in self.packets_to_send:
@@ -93,14 +36,32 @@ class Pinger(Job):
 			return True
 		return False
 
-	def process(self, data: bytes, address: SocketAddress) -> None:
-		ip_h, icmp_h = icmp.ICMPv4.parse(data)
-		if icmp_h.type == TYPE_ECHO_REPLY:
-			self.report.set_reached(ip_h.source, ip_h.ttl)
+	def process(self, data: bytes, address: SocketAddress) -> Tuple[bool, IPv4Address, int]:
+		ip_h, icmp_h = ICMPv4.parse(data)
+		if address[0] not in self._cache and icmp_h.type == TYPE_ECHO_REPLY:
+			self._count -= 1
+			self._cache.add(address[0])
+			return True, IPv4Address(address[0]), ip_h.ttl
 		if icmp_h.type == TYPE_DESTINATION_UNREACHABLE:
-			original_ip_frame = IPv4.from_bytes(icmp_h.data)
-			if original_ip_frame.is_checksum_valid():
-				self.report.set_unreachable(original_ip_frame.destination)
+			ip_frame = IPv4.from_bytes(icmp_h.data)
+			destination = IPv4Address(ip_frame.destination)
+			if destination not in self._cache and ip_frame.is_checksum_valid():
+				self._count -= 1
+				self._cache.add(str(destination))
+		if self._count < 1:
+			self.set_finished()
+
+	def get_missing_hosts(self) -> set:
+		return set(map(lambda x: str(x), self.subnet.hosts())) - self._cache
+
+	@classmethod
+	def generate_echo_requests(cls, subnet: IPv4Network, repeat: int) -> Iterator[Tuple[bytes, SocketAddress]]:
+		for _ in range(0, repeat):
+			for address in subnet.hosts():
+				t = ICMPv4.echo(b"abcdefghijklmonpqrstuvwxyz")
+				t.set_checksum()
+				yield bytes(t), (str(address), 0)
+		return
 
 
 if __name__ == "__main__":
@@ -108,21 +69,34 @@ if __name__ == "__main__":
 	import sys
 
 
+	# Parsing arguments
 	if len(sys.argv) < 2:
 		print("Too few parameters")
+		exit(0)
+	if len(sys.argv) > 2 and sys.argv[2] == "-m":
+		print("[INFO] Script will wait additional ten seconds for destination unreachable messages.")
+		print_missing = True
+		wait = 10
 	else:
-		report = NetworkReport(sys.argv[1])
-		scanner = Pinger(
-			socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP),
-			report,
-			interval=0.1
-		)
-		print("Scanning..")
-		scanner.run()
-		print("Hosts found:")
-		for i in report.get_confirmed_host():
-			print(i)
-		print("Hosts for which a destination unreachable message wasn't received:")
-		for i in report.get_missing_host():
-			print(i)
+		print_missing = False
+		wait = 0.5
+	# Create Job
+	job = Pinger(
+		socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP),
+		ip_network(sys.argv[1]),
+		interval=0.05,
+		wait=wait
+	)
+	# Ping
+	for flag, ip, ttl in job.stream():
+		if flag:
+			print(f"{ip}\t{ttl}")
+	# Missing
+	if print_missing:
+		miss = job.get_missing_hosts()
+		if len(miss) > 0:
+			print("[INFO] Hosts for which a destination unreachable message wasn't received:")
+			print("\n".join(map(lambda x: f"\t - {x}", miss)))
+		else:
+			print("[INFO] Destination unreachable message was received for all missing host.")
 

